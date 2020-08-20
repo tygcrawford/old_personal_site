befa5bc357c3e89dc5abaa02b8d4e746d1edc5a4
Ty Crawford <tygcrawford@gmail.com>
2020-08-20 15:59:53 -0400

    Added mobile compatible design

--- public/main.js
+++ public/main.js
@@ -1,32 +1,34 @@
-// loading icon
+// scroll to top for animation
 window.onbeforeunload = () => { this.scrollTo(0,0) };
 
-window.onload = () => {
-    let speed = 2000;
+// // loading animation
+// window.onload = () => {
+//     let speed = 2000;
 
-    $("body").css("overflow", "hidden");
+//     $("body").toggleClass("scroll-lock");
 
-    setTimeout(() => {
-        $("#loading-overlay").fadeOut(500, () => {
-            $("body").css("overflow", "scroll");
-        });
-    }, speed + 500);
+//     setTimeout(() => {
+//         $("#loading-overlay").fadeOut(500, () => {
+//             $("body").toggleClass("scroll-lock");
+//         });
+//     }, speed + 500);
 
-    $("#toprect").animate({
-		y: "+=30",
-		x: "-=30"
-	}, speed);
-	$("#bttmrect").animate({
-		y: "-=30",
-		x: "+=30"
-    }, speed);
-}
+//     $("#toprect").animate({
+// 		y: "+=30",
+// 		x: "-=30"
+// 	}, speed);
+// 	$("#bttmrect").animate({
+// 		y: "-=30",
+// 		x: "+=30"
+//     }, speed);
+// }
 
 $(() => {
     $("#hamburger-containter").click(() => {
         $("#hamburger-containter").toggleClass("hamburger-open");
         $("#nav-slider").toggleClass("slider-open");
         $("#dark-overlay").fadeToggle(350);
+        $("body").toggleClass("scroll-lock")
     });
 });
 
